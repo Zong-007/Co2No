aa2f5a009aba565d95d2fa0444e5a57e4c41292d
Zongkung <nontasit2411@gmail.com>
2025-01-11 13:27:41 +0700

    coneting-php

--- export.php
+++ export.php
@@ -1,9 +1,9 @@
 <?php
 // ข้อมูลการเชื่อมต่อฐานข้อมูล
-$host = "sql202.infinityfree.com"; // MySQL Hostname
-$username = "if0_37872897"; // MySQL Username
-$password = "iwjuMyabV2NICh"; // MySQL Password
-$dbname = "if0_37872897_projectoxy"; // ชื่อฐานข้อมูลจริง
+$host = "irkm0xtlo2pcmvvz.chr7pe7iynqr.eu-west-1.rds.amazonaws.com"; // MySQL Hostname
+$username = "atr8951bo6cc30cs"; // MySQL Username
+$password = "qs7uojv84spfdsv6"; // MySQL Password
+$dbname = "g01br52t9mmmpfah"; // ชื่อฐานข้อมูล (เปลี่ยน XXX เป็นชื่อฐานข้อมูลจริง)
 $port = 3306; // MySQL Port (ค่าเริ่มต้นคือ 3306)
 
 // สร้างการเชื่อมต่อ
